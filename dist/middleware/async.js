0c25d11aa33835432ec0e37d2f6077b55736ee29
asimhyff <asimhyff@users.noreply.github.com>
2020-09-11 13:55:45 +0500

    updated

--- dist/middleware/async.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = void 0;
-// interface asyncHandlerI {
-//   func: (req: Request, res: Response, next: NextFunction) => void;
-// }
-exports.asyncHandler = (func) => (req, res, next) => {
-    Promise.resolve(func(req, res, next)).catch(next);
-};
